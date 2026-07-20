723f0f2aab5a6d6d14da0ae594777f28e215b158
BIMONI Dev <info@mkgrp.biz>
2026-07-21 01:41:11 +0900

    トップバーの「TSUNAGU 管理画面」テキストを削除
    サイドバーにロゴがあるため不要。

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -121,7 +121,6 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
             </button>
-            <span class="font-semibold">TSUNAGU 管理画面</span>
         </div>
 
         <main class="px-6 py-8">
